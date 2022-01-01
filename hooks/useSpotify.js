3fe50fef1562b917d95f28296ae8c91efeef2ef8
Hamza ARROUBAT <ht.arroubat@gmail.com>
2022-01-01 12:29:39 +0100

    implementing the spotify custom hook

---
+++ hooks/useSpotify.js
@@ -0,0 +1,18 @@
+import { signIn, useSession } from 'next-auth/react'
+import spotifyApi from '../lib/spotify'
+
+export default function useSpotify() {
+  const { data: session, status } = useSession()
+
+  useEffect(() => {
+    if (session) {
+      if ((session.error = 'RefreshAccessTokenError')) {
+        signIn()
+      }
+    }
+
+    spotifyApi.setAccessToken(session.user.accessToken)
+  }, [session])
+
+  return spotifyApi
+}
